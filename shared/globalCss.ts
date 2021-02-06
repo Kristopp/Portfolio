eb56366e84b02f84051e89251316aa2880fb9796
Kristopp <kristo.palu89@gmail.com>
2021-02-06 09:56:44 +0200

    get rdy for development

---
+++ shared/globalCss.ts
@@ -0,0 +1,36 @@
+import { css } from "@emotion/react";
+
+export const globalStyles = css`
+  :root {
+    --page-width: 1000px;
+    --page-background: rgb(40, 40, 40);
+    --page-text: #fff;
+    --background-accent: rgb(34, 34, 34);
+    --color-text-invert: rgb(240, 240, 240);
+    --color-accent: rgb(4, 64, 100);
+    --reading-width: 60ch;
+    --white: #fff;
+  }
+
+  html,
+  body {
+    height: 100%;
+    padding: 10;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    background-color: var(--page-background);
+    color: var(--page-text);
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  *,
+  *::after,
+  *::before {
+    box-sizing: border-box;
+  }
+`;
