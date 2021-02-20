68af7409c8ba11462a5d8c476b3028814122afa1
Kristopp <kristo.palu89@gmail.com>
2021-02-20 14:43:06 +0200

    Working on email modal

---
+++ components/emailComponent/index.tsx
@@ -0,0 +1,18 @@
+
+type EmailProps = {
+  email: string;
+  text: string;
+  openCloseState: boolean
+};
+
+const EmailComponent = () => { 
+  const 
+  return ( 
+    <div>
+      <input type="text"  defaultValue="title"/>
+      <input type="text" defaultValue="what is ur email" />
+    </div>
+  )
+}
+
+export default EmailComponent;
