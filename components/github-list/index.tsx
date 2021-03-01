d111798947b066dfe19327f6e9851b69de263eda
Kristopp <kristo.palu89@gmail.com>
2021-03-01 18:44:01 +0200

    daily com

---
+++ components/github-list/index.tsx
@@ -0,0 +1,8 @@
+type GitData = { 
+    git_url: string,
+    createdAt: string,
+    description: string
+}
+type Props = {
+ data: GitData[]
+};
--- pages/work.tsx
+++ pages/work.tsx
@@ -1,14 +1,30 @@
-import { GetStaticProps } from 'next'
-export const Work = () => { 
-    return ( 
-        <div>
+import { GetStaticProps } from "next";
+import { useState } from "react";
 
-        </div>
-    )
+const GIT_USER = process.env.NEXT_PUBLIC_GIT_USER;
+const GIT_HUB_LINK = `https://api.github.com/users/${GIT_USER}/repos` ;
+
+const query= { 
+sort: "full"
 }
 
-export const getStaticProps = async () => {
-    // ...
-  }
+export const Work = ({ ...data }: Props) => {
 
-export  default Work
+
+  return <div>}</div>;
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch(GIT_HUB_LINK,);
+  const data = await res.json();
+  if (!data) {
+    return {
+      notfound: true,
+    };
+  }
+  return {
+    props: { data }, // will be passed to the page component as props
+  };
+};
+
+export default Work;
